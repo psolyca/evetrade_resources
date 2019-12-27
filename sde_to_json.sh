8f69d6cb9e245ae8d8586bd9c499375dd51355a1
awhipp <alexruns4@gmail.com>
2019-12-27 16:34:51 -0500

    pages push

--- sde_to_json.sh
+++ sde_to_json.sh
@@ -17,13 +17,3 @@ echo 'Latest resources revision was :' $lastrev
 echo 'Building resources files for new revision' $currentrev
 python3 toJSON.py
 echo "export gittag=$currentrev" > gittag
-
-# config
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
-
-git init
-git add .
-git commit -m "Deploy to Github Pages"
-git push --force "https://${GH_TOKEN}@github.com/evetrade_resources.git" master:gh-pages
-
